0adb0ca8fbd51d048b8ff4f0868906181de3ff17
yulvjun <lvjun.yu@yulvjundeMacBook-Pro.local>
2020-10-27 22:51:27 +0800

    [ylj]: add student

--- src/components/Student.jsx
+++ src/components/Student.jsx
@@ -5,6 +5,7 @@ import './Student.css';
 class Student extends Component {
   state = {
     students: {},
+    inputName: ''
   };
 
   async componentDidMount() {
@@ -20,6 +21,29 @@ class Student extends Component {
 
   }
 
+  async handleConfirmAddStudent(){
+    const response = await fetch("http://localhost:8080/student/add",{
+      method:"POST",
+      body:this.state.inputName
+    })
+    const data = await response.json();
+    this.setState({
+      students: data,
+    });
+  }
+
+  handleCancelAddStudent(){
+    this.setState({
+      inputName: ""
+    });
+  }
+
+  handleFileChange = (event) => {
+    this.setState({
+      inputName: event.target.value,
+    })
+  };
+
 
 render()
 {
@@ -30,13 +54,21 @@ render()
         {
           Object.values(this.state.students).map((student, index) => {
             return <tr key={index}>
-              <label>{index+1}. </label>
+              <label>{student.id}. </label>
               <td>{student.name}</td>
              </tr>
           })
         }
-        <button type="button" onClick={(e) => this.handleAddStudent( e)}>添加</button>
-        
+        {/*<button type="button" onClick={(e) => this.handleAddStudent( e)}>添加</button>*/}
+        <div>
+          <input
+              type="text"
+              onChange={(e) => this.handleFileChange(e)}
+              placeholder="请输入要添加的姓名"
+          />
+          <button type="button" onClick={(e) => this.handleConfirmAddStudent( e)}>确认添加</button>
+          {/*<button type="button" onClick={(e) => this.handleCancelAddStudent( e)}>取消</button>*/}
+        </div>
       </div>
 
     </div>
