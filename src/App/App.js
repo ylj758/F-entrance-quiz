349fef4265e1254e1a6b99593e4693a937052a0f
kevindongzg <dongkevin94@gmail.com>
2020-11-01 20:35:29 +0800

    add TODOs

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
 import './App.scss';
-import Student from '../components/Student'
+import Student from '../components/Student';
 
+// TODO GTB-1: * 页面没有样式
+// TODO GTB-1: * 只是实现了添加学员和学员列表，且添加形式没有符合需求
+// TODO GTB-2: * 没有测试
+// TODO GTB-3: * 没有拆分组件/只有一个Student（单数）组件，思考如何拆分与复用组件
+// TODO GTB-3: * 语义化标签使用不好
+// TODO GTB-3: * 没有写样式
+// TODO GTB-3: * 运用了ES6+语法及fetch，但完成度较低一些知识点无法验证
+// TODO GTB-3: * 运用React相关知识点，但完成度较低一些知识点无法验证
+// TODO GTB-4: * 有小步提交意识，但message和小步粒度还需提升
+// TODO GTB-4: * 没有抽出Api请求层
+// TODO GTB-4: * 组件命名，拆分，抽象，复用还需提升
+// TODO GTB-4: * 完成度较低，部分工程化实践无法验证
 class App extends Component {
   render() {
     return (
       <div data-testid="app" className="App">
-        <Student/>
+        <Student />
       </div>
     );
   }
--- src/components/Student.jsx
+++ src/components/Student.jsx
@@ -1,10 +1,13 @@
 import React, {Component} from 'react'
+// TODO GTB-4: - 这里不用独立引入@babel/polyfill，如有需要请修改 .babelrc文件
 import '@babel/polyfill';
 import './Student.css';
 
+// TODO GTB-4: - 这里组件命名是应该单数的Student吗？
 class Student extends Component {
   state = {
     students: {},
+    // TODO GTB-4: - 冗余的state inputName，这里不用做双向绑定
     inputName: ''
   };
 
@@ -32,27 +35,32 @@ class Student extends Component {
     });
   }
 
+  // TODO GTB-4: - 同理，不用双向绑定，这个方法也是冗余的
   handleCancelAddStudent(){
     this.setState({
       inputName: ""
     });
   }
 
+  // TODO GTB-4: - 同理，不用双向绑定，这个方法也是冗余的，且注意命名，怎么会是File？
   handleFileChange = (event) => {
     this.setState({
       inputName: event.target.value,
     })
   };
 
-
+// TODO GTB-4: - 注意缩进
 render()
 {
   return (
+    // TODO GTB-4: - 加强语义化标签的使用 
     <div className="order">
       <div>
         <h2>学员列表</h2>
         {
+          // TODO GTB-4: - 数组直接map即可，不用Object.values
           Object.values(this.state.students).map((student, index) => {
+            // TODO GTB-4: - 不推荐使用index作为key，可以使用id等
             return <tr key={index}>
               <label>{student.id}. </label>
               <td>{student.name}</td>
@@ -63,9 +71,11 @@ render()
         <div>
           <input
               type="text"
+              // TODO GTB-4: - 不用双向绑定，这里onChange可以改成onKeyUp/onKeyDown
               onChange={(e) => this.handleFileChange(e)}
               placeholder="请输入要添加的姓名"
           />
+          {/* TODO GTB-3: - 这里下面一般直接写{this.handleConfirmAddStudent}，定义handleConfirmAddStuden成箭头函数即可 */}
           <button type="button" onClick={(e) => this.handleConfirmAddStudent( e)}>确认添加</button>
           {/*<button type="button" onClick={(e) => this.handleCancelAddStudent( e)}>取消</button>*/}
         </div>
